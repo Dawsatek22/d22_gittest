247eeb9df86a9bd88ac745390885f0b7c36729a5
vlaam+5@tutanota.com <vlaam+5@tutanota.com>
2026-07-02 11:51:04 +0200

    add inverse kinematics2

---
+++ kinematics/ik/dof1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+
+double  pos(double y1,double x1) {
+    
+ double pos = atan2(y1,x1) * (180/3.1415) ;
+
+return pos;
+
+}
+
+
+int main() {
+
+
+int pos1 = 90;
+std::cout <<"the angle position is:" <<  pos(7.5,-4.5) << '\n';
+return 0;
+
+}
